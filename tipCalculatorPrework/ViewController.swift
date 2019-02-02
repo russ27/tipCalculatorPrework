ea39b80dcd678f6710bc41fc950928fbf914ed61
Russelle Pineda <russellepineda@Russelles-Mac.local>
2019-02-01 21:41:21 -0800

    Initial Commit

---
+++ tipCalculatorPrework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculatorPrework
+//
+//  Created by Russelle Pineda on 2/1/19.
+//  Copyright © 2019 Russelle Pineda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
